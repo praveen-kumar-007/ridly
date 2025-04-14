661c90b9bc329176011387e02523c1862d7c0421
praveen-kumar-007 <praveen.pr105@gmail.com>
2025-04-15 01:13:29 +0530

    my music

--- src/spotify.js
+++ src/spotify.js
@@ -4,7 +4,7 @@
 const authEndpoint = "https://accounts.spotify.com/authorize?";
 const clientId = "14ddf12b0d754cb59dfa8d37247dd792"; // Your client id
 // const clientSecret = "64f5037ca4b546debd8644443090dbf1"; // Your secret
-const redirectUri = "https://localhost:3000/callback"; // Your redirect uri
+const redirectUri = "https://ridly-taupe.vercel.app/"; // Your redirect uri
 const scopes = ["user-library-read", "playlist-read-private"];
 
 
